00607600f8306a47aa461cd81064ebd85be48255
josemorenoasix <josemorenoasix@gmail.com>
2017-03-17 07:45:38 +0100

    Update template-nss_mysql.sql

--- template-nss_mysql.sql
+++ template-nss_mysql.sql
@@ -8,7 +8,7 @@ CREATE TABLE groups (
   password varchar(64) DEFAULT 'x' NOT NULL,
   flag char(1) DEFAULT 'A'
 );
-INSERT INTO groups VALUES (1,'users',100,'x','A');
+INSERT INTO groups VALUES (1,'users',100,'template.group.users.passwd','A');
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
