5b872dfd6fcf83ee1e003b1da8d082190654074f
josemorenoasix <josemorenoasix@gmail.com>
2017-03-19 06:24:19 +0100

    Update template-nss_mysql.sql

--- template-nss_mysql.sql
+++ template-nss_mysql.sql
@@ -1,8 +1,6 @@
-
-use nss-auth;
+use template.nss.dbname;
 
 # Las tablas ...
-DROP TABLE IF EXISTS groups;
 CREATE TABLE groups (
   name varchar(20) NOT NULL default '',
   password varchar(34) NOT NULL default 'x',
@@ -10,7 +8,6 @@ CREATE TABLE groups (
   PRIMARY KEY  (gid)
 ) TYPE=MyISAM AUTO_INCREMENT=5000;
 
-DROP TABLE IF EXISTS grouplist;
 CREATE TABLE grouplist (
   rowid smallint NOT NULL auto_increment,
   gid smallint NOT NULL default '0',
@@ -18,7 +15,6 @@ CREATE TABLE grouplist (
   PRIMARY KEY  (rowid)
 ) TYPE=MyISAM;
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   username varchar(20) NOT NULL default '',
   uid smallint NOT NULL auto_increment,
@@ -43,24 +39,23 @@ GRANT USAGE ON *.* TO `template.nss.admin`@`localhost` IDENTIFIED BY 'template.n
 GRANT USAGE ON *.* TO `template.nss.user`@`localhost` IDENTIFIED BY 'template.nss.user.passwd';
 
 GRANT Select (`username`, `uid`, `gid`, `shell`, `password`)
-             ON `nss-auth`.`users`
+             ON `template.nss.dbname`.`users`
              TO 'template.nss.admin'@'localhost';
 GRANT Select (`name`, `password`, `gid`)
-             ON `nss-auth`.`groups`
+             ON `template.nss.dbname`.`groups`
              TO 'template.nss.admin'@'localhost';
 
 GRANT Select (`username`, `uid`, `gid`, `shell`)
-             ON `nss-auth`.`users`
+             ON `template.nss.dbname`.`users`
              TO 'template.nss.user'@'localhost';
 GRANT Select (`name`, `password`, `gid`)
-             ON `nss-auth`.`groups`
+             ON `template.nss.dbname`.`groups`
              TO 'template.nss.user'@'localhost';
 
 GRANT Select (`username`, `gid`)
-             ON `nss-auth`.`grouplist`
+             ON `template.nss.dbname`.`grouplist`
              TO 'template.nss.user'@'localhost';
 GRANT Select (`username`, `gid`)
-             ON `nss-auth`.`grouplist`
+             ON `template.nss.dbname`.`grouplist`
              TO 'template.nss.admin'@'localhost';
-			 
 FLUSH PRIVILEGES;
