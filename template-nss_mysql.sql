a529ff45b71f79f85a46b56f204ebe6938e09995
josemorenoasix <josemorenoasix@gmail.com>
2017-03-19 07:33:04 +0100

    Update template-nss_mysql.sql

--- template-nss_mysql.sql
+++ template-nss_mysql.sql
@@ -1,62 +1,61 @@
-use template.nss.dbname;
+USE nss_mysql;
 
-# Las tablas ...
+DROP TABLE IF EXISTS groups;
 CREATE TABLE groups (
-  name varchar(20) NOT NULL default '',
-  password varchar(34) NOT NULL default 'x',
-  gid smallint NOT NULL auto_increment,
-  PRIMARY KEY  (gid)
-) ENGINE=MyISAM AUTO_INCREMENT=5000;
+  dbid int(11) NOT NULL auto_increment primary key,
+  name varchar(30) DEFAULT '' NOT NULL,
+  gid int(11) NOT NULL,
+  password varchar(64) DEFAULT 'x' NOT NULL,
+  flag char(1) DEFAULT 'A'
+);
+INSERT INTO groups VALUES (1,'users',100,'SHA('users_pass')','A');
 
-CREATE TABLE grouplist (
-  rowid smallint NOT NULL auto_increment,
-  gid smallint NOT NULL default '0',
-  username char(16) NOT NULL default '',
-  PRIMARY KEY  (rowid)
-) ENGINE=MyISAM;
+DROP TABLE IF EXISTS users;
+/* 
 
+http://libnss-mysql.sourceforge.net/libnss-mysql/sample/linux/sample_database.sql
+http://www.tldp.org/LDP/lame/LAME/linux-admin-made-easy/shadow-file-formats.html 
+
+*/
 CREATE TABLE users (
-  username varchar(20) NOT NULL default '',
-  uid smallint NOT NULL auto_increment,
-  gid smallint NOT NULL default '5000',
-  shell varchar(64) NOT NULL default '/bin/bash',
-  password varchar(34) NOT NULL default 'x',
-  PRIMARY KEY (uid),
-  UNIQUE KEY username (username),
-  KEY uid (uid)
-) ENGINE=MyISAM AUTO_INCREMENT=5000;
+  dbid int(11) NOT NULL auto_increment primary key,
+  username varchar(50) DEFAULT '' NOT NULL,
+  gecos varchar(40) DEFAULT '' NOT NULL,
+  shell varchar(20) DEFAULT '/bin/sh' NOT NULL,
+  password varchar(60) DEFAULT '' NOT NULL,
+  flag char(1) DEFAULT 'N' NOT NULL,
+  uid int(11) NOT NULL,
+  gid int(11) NOT NULL,
+  homedir varchar(64) DEFAULT '/bin/sh' NOT NULL,
+  lstchg varchar(50) NOT NULL default '',
+  min int(11) NOT NULL default '0',
+  max int(11) NOT NULL default '0',
+  warn int(11) NOT NULL default '7',
+  inact int(11) NOT NULL default '-1',
+  expire int(11) NOT NULL default '-1'
+);
 
-# Algunos datos ...
-INSERT INTO users (username, password)
-    VALUES ('cinergi', SHA1('cinergi'));
-INSERT INTO groups (name)
-    VALUES ('ssh-mysql');
-INSERT INTO grouplist (gid,username)
-    VALUES (5000,'cinergi');
+DROP TABLE IF EXISTS grouplist;
+CREATE TABLE grouplist (
+  dbid int(11) NOT NULL auto_increment primary key,
+  gid int(11) DEFAULT '0' NOT NULL,
+  uid int(11) DEFAULT '0' NOT NULL,
+  username varchar(50) DEFAULT '' NOT NULL
+);
 
-# Los permisos ...
-GRANT USAGE ON *.* TO `template.nss.admin`@`localhost` IDENTIFIED BY 'template.nss.admin.passwd';
-GRANT USAGE ON *.* TO `template.nss.user`@`localhost` IDENTIFIED BY 'template.nss.user.passwd';
+GRANT select(dbid,username,uid,gid,gecos,shell,homedir,flag) on users to 'template.nss.user'@localhost identified by 'template.nss.user.passwd';
+GRANT select(dbid,name,gid,password,flag) on groups to 'template.nss.user'@localhost identified by 'template.nss.user.passwd';
+GRANT select(dbid,gid,uid,username) on grouplist to 'template.nss.user'@localhost identified by 'template.nss.user.passwd';
+GRANT select(dbid,username,password,uid,gid,gecos,shell,homedir,flag,lstchg,min,max,warn,inact,expire) on users to 'template.nss.user'@localhost identified by 'template.nss.user.passwd';
+GRANT select(dbid,username,uid,gid,gecos,shell,homedir,flag) on users to 'template.nss.admin'@localhost identified by 'template.nss.admin.passwd';
+GRANT select(dbid,name,gid,password,flag) on groups to 'template.nss.admin'@localhost identified by 'template.nss.admin.passwd';
+GRANT select(dbid,gid,uid,username) on grouplist to 'template.nss.admin'@localhost identified by 'template.nss.admin.passwd';
+GRANT select(dbid,username,password,uid,gid,gecos,shell,homedir,flag,lstchg,min,max,warn,inact,expire) on users to 'template.nss.admin'@localhost identified by 'template.nss.admin.passwd';
+GRANT update(dbid,username,password,uid,gid,gecos,shell,homedir,flag,lstchg,min,max,warn,inact,expire) on users to 'template.nss.admin'@localhost identified by 'template.nss.admin.passwd';
 
-GRANT Select (`username`, `uid`, `gid`, `shell`, `password`)
-             ON `template.nss.dbname`.`users`
-             TO 'template.nss.admin'@'localhost';
-GRANT Select (`name`, `password`, `gid`)
-             ON `template.nss.dbname`.`groups`
-             TO 'template.nss.admin'@'localhost';
-
-GRANT Select (`username`, `uid`, `gid`, `shell`)
-             ON `template.nss.dbname`.`users`
-             TO 'template.nss.user'@'localhost';
-GRANT Select (`name`, `password`, `gid`)
-             ON `template.nss.dbname`.`groups`
-             TO 'template.nss.user'@'localhost';
-
-GRANT Select (`username`, `gid`)
-             ON `template.nss.dbname`.`grouplist`
-             TO 'template.nss.user'@'localhost';
-GRANT Select (`username`, `gid`)
-             ON `template.nss.dbname`.`grouplist`
-             TO 'template.nss.admin'@'localhost';
-          
 FLUSH PRIVILEGES;
+
+INSERT INTO groups VALUES (2,'testing',10000,SHA('1234'),'A');
+INSERT INTO users VALUES (1,'testing','testing purpose','/bin/bash',SHA('1234'),'A',10000,10000,'/home/testing','','0','0','7','-1','-1');
+INSERT INTO grouplist VALUES (1,10000,10000,'testing');
+INSERT INTO grouplist VALUES (2,100,10000,'testing');
